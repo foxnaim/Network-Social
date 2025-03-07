52b51027a815b91fe0d052f6bdb5e63bc5d7533f
Rick <118506963+foxnaim@users.noreply.github.com>
2025-03-07 19:35:40 +0500

    modified:   app.js 	modified:   package.json

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ app.use("/uploads", express.static('uploads'))
 
 app.use('/api', require("./routes"))
 
-if(fs.existsSync('uploads')){
-  fs.mkdirSync('uploads')
+if (!fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
 }
+
 
 
 // catch 404 and forward to error handler
