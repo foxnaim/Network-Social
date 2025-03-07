500e2e9aa5ae1a3edf5428d080deda51e5641259
Rick <118506963+foxnaim@users.noreply.github.com>
2025-03-07 14:58:19 +0500

    modified:   app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.set('view engine', 'jade');
+
 app.use("/uploads", express.static('uploads'))
 
 
