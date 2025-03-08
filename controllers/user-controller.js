600c8d02cf4cdf7ce27869ec6e8b7685e0a1dcee
Rick <118506963+foxnaim@users.noreply.github.com>
2025-03-08 22:27:34 +0500

    modified:   controllers/user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,26 +1,32 @@
+const { prisma } = require("../prisma/prisma-client");
+
 const UserController = {
- register: async (req, res) =>{
- const {
-  email,
- password,
- name,
- } = req.body
- console.log(email,
-  password,
-  name,)
- },
- login: async (req, res) =>{
-  res.send("Login")
- },
- getUserById: async (req, res) =>{
-  res.send("getUserById")
- },
- updateUser: async (req, res) =>{
-  res.send("updateUser")
- },
- current: async (req, res) =>{
-  res.send("current")
- },
-}
+  register: async (req, res) => {
+    const { email, password, name } = req.body;
+
+    if (!email || !password || !name){
+     return res.status(400).json({error: "Все поля обязательны"})
+    } try{
+     const existingUser = await prisma.user.findUnique(({where:{email}}));
+    if(existingUser){
+     return res.status(400).json({error: "Пользователь уже существует"})
+   }
+  }catch(error){
+
+   
+  },
+  login: async (req, res) => {
+    res.send("Login");
+  },
+  getUserById: async (req, res) => {
+    res.send("getUserById");
+  },
+  updateUser: async (req, res) => {
+    res.send("updateUser");
+  },
+  current: async (req, res) => {
+    res.send("current");
+  },
+};
 
 module.exports = UserController;
